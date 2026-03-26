cb0639661aeee9236a3c38900472d3bdb9a2ac08
Vladimir Svyatski <svp@scand.com>
2026-03-26 15:00:06 +0300

    Should not be casted to BOOL

--- w32/main.c
+++ w32/main.c
@@ -341,7 +341,7 @@ dlg_proc (HWND dlg, UINT msg, WPARAM wparam, LPARAM lparam)
           /* Display the error prompt in red.  */
           SetTextColor ((HDC)wparam, RGB (255, 0, 0));
           SetBkMode ((HDC)wparam, TRANSPARENT);
-          return (BOOL)(GetStockObject(NULL_BRUSH) != NULL);
+          return (INT_PTR)GetStockObject(NULL_BRUSH);
         }
       break;
 
