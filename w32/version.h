b2b558e225dc5b83fd08946381dd5b25995fc8b6
Vladimir Svyatski <svp@scand.com>
2026-03-26 10:19:32 +0300

    Adding version

---
+++ w32/version.h
@@ -0,0 +1,10 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define APP_VER			1,3,3,0
+#define	WINGPG_VER		1,2,0,0
+
+#define APP_VER_STR		"1.3.3.0"
+#define WINGPG_VER_STR	"1.2.0.0"
+
+#endif	//VERSION_H
